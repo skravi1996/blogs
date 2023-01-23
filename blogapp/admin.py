227808a05d6d800101587b252410d6e9c2bdb570
sk.ravi1996 <sk.ravi1996.com>
2023-01-24 00:55:28 +0530

    new commit_2

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,30 +1,8 @@
 from django.contrib import admin
 from . import models
-from .models import Blog2
+from .models import Blog2,Blog
 
 # Register your models here.
 
 admin.site.register(Blog2)
-@admin.register(models.Blog)
-
-# The BlogAdmin class is a subclass of the admin.ModelAdmin class. 
-class BlogAdmin(admin.ModelAdmin):
-    
-    fields = [
-        
-        'user',
-        'title',
-        'blog_text',
-        'main_image',
-    ]
-    list_display = [
-        'title', 
-        'user',
-        'blog_text',
-        'main_image',
-        'created_at',
-        'updated_at',
-        
-    ]
-
-
+admin.site.register(Blog)
