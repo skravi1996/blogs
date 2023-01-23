03e83b7834e498d4117499692d51ee2e21bea34d
sk.ravi1996 <sk.ravi1996.com>
2023-01-24 01:00:14 +0530

    new commit_3

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,27 +3,16 @@ import uuid
 from django.contrib.auth.models import User
 
 
-
-# > This class is an abstract class that will be inherited by all other models. It will add a UUID
-# field, a created_at field, and an updated_at field to all models that inherit from it
-class BaseModel(models.Model):
-    uid =models.UUIDField(primary_key=True, editable= False, default= uuid.uuid4)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    
-    class Meta:
-        abstract = True
-        
   
 # The Blog class inherits from the BaseModel class, and has a one-to-many relationship with the User class      
-class Blog(BaseModel):
+class Blog(models.Model):
     user =  models.ForeignKey(User, related_name='blogs', on_delete=models.CASCADE)
     title_2 = models.CharField(max_length=200)
     blog_text_2= models.TextField()  
     main_image = models.FileField()   
         
     def __str__(self):
-        return self.title
+        return self.title_2
 
 
 class Blog2(models.Model):
@@ -32,4 +21,4 @@ class Blog2(models.Model):
     main_image = models.FileField()   
         
     def __str__(self):
-        return self.title
+        return self.title_1
